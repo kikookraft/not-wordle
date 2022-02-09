aa43c4c4fa7a6be2d586d58d1df355107519ac43
kikookraft <kikookraft.teamcraft@gmail.com>
2022-02-09 16:17:22 +0100

    Ajout des fonctions pour créer la grilles de lettres

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from tkinter import font
+## Written in python 3.8.5 with pygame 2.0.0
+
 import pygame
 import pygame_gui
 import random
@@ -8,7 +9,7 @@ class game():
         game.screen_size=(900,800)
         game.w = self.screen_size[0]
         game.h = self.screen_size[1]
-        game.bg_color='#161E1E'
+        game.bg_color='#0A0A0A'
         game.FPS = 60
         game.window_surface = pygame.display.set_mode(self.screen_size, pygame.RESIZABLE)
         game.background = pygame.Surface(self.screen_size)
@@ -19,11 +20,15 @@ class game():
         pygame.display.set_caption('Not Wordle')
         game.manager = pygame_gui.UIManager(self.screen_size)
         game.texts = {}
+        game.rect = {}
         game.font = pygame.font.SysFont("res/font/AnonymousPro-Bold.ttf", 24)
         game.tick = 0
         game.started = False
         game.word = ""
         game.guess = []
+    
+    def beizer(self, x):
+        return x**2*(3-2*x)
 
     def menu_btn(self):
         #self.screen_size = (self.window_surface.get_width(), self.window_surface.get_height())
@@ -47,15 +52,17 @@ class game():
             game.texts[str(id)]=(tx, (pos[0]*game.w-(txt_size[0]/2), pos[1]*game.h-(txt_size[1]/2)))
         else:
             game.texts[str(id)]=(tx, (pos[0]*game.w, pos[1]*game.h))
+        return str(id)
 
     def update(self):
         self.screen_size = (self.window_surface.get_width(), self.window_surface.get_height())
         game.window_surface = pygame.display.set_mode(self.screen_size, pygame.RESIZABLE)
-        pygame.draw.rect(self.window_surface, (22,30,30), (0,0,self.screen_size[0], self.screen_size[1]))
+        pygame.draw.rect(self.window_surface, (10,10,10), (0,0,self.screen_size[0], self.screen_size[1]))
         game.manager = pygame_gui.UIManager(self.screen_size)
-        for i in self.btn:
-            i.kill()
-        self.menu_btn()
+        if not self.started:
+            for i in self.btn:
+                i.kill()
+            self.menu_btn()
     
     def start(self):
         self.button_play.kill()
@@ -68,19 +75,38 @@ class game():
                 l.append(line.rstrip())
         game.word = random.choice(l)
         l.clear()
-        self.text((0.5, 0.1), game.word, 1, (0,200,0), 75)
-        
+        self.texts.clear()
+        #self.text((0.5, 0.1), game.word, 1, (0,200,0), 75)
+        print(self.word)
+        self.game_ui()
         self.started = True
 
+    def draw_rect(self, id, pos, size, color=(30,30,30), keep_ratio = False):
+        self.rect[str(id)] = {'pos':pos, 'size':size, 'color':color, 'ratio':keep_ratio}
+        return str(id)
 
-        
+    def game_ui(self):
+        box_size = 0.8/(len(game.word)*1.33)
+        space_between = box_size/8
+        space_below = box_size/2
+        lines = len(game.word)
+        y_offset = 0.5-(box_size*lines+space_below*lines)/2
+        for i in range(lines+1):
+            x_offset = 0.5-(box_size*lines+space_between*lines)/2
+            for j in range(lines):
+                self.draw_rect("{} {}".format(i,j), (x_offset,y_offset),(box_size,box_size),keep_ratio=True)
+                x_offset+= box_size+space_between
+            y_offset+=box_size+space_below
+
 
 if __name__ == "__main__":
     G = game()
     G.menu_btn()
     G.update()
     G.button_words.disable()
-    G.text((0.01,0.01), "By kikookraft", 0, centered=False)
+    kik = G.text((0.01,0.01), "By kikookraft", 0, centered=False)
+    title = G.text((0.5,0.1), "Defenitly not Wordle", 1, size=50)
+
 
     while G.is_running:
         time_delta = G.clock.tick(G.FPS)/1000.0
@@ -99,7 +125,12 @@ if __name__ == "__main__":
             if event.type == pygame.VIDEORESIZE:
                 surface = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
                 G.screen_size = (event.w, event.h)
+                G.w = event.w
+                G.h = event.h
                 G.update()
+                G.button_words.disable()
+                try:del G.texts[kik]
+                except:pass
             G.manager.process_events(event)
 
         G.manager.update(time_delta)
@@ -107,5 +138,13 @@ if __name__ == "__main__":
         G.manager.draw_ui(G.window_surface)
         for txt in G.texts:
             G.window_surface.blit(G.texts[txt][0], G.texts[txt][1])
+        for rect in G.rect:
+            x= G.rect[rect]['pos'][0]*G.w - G.rect[rect]['size'][0]*G.w/2 ## centrer en x
+            y= G.rect[rect]['pos'][1]*G.h - G.rect[rect]['size'][1]*G.h/2 ## centrer en y
+            if G.rect[rect]['ratio']:
+                box = pygame.Rect(x, y, G.rect[rect]['size'][1]*G.h, G.rect[rect]['size'][1]*G.h)
+            else:
+                box = pygame.Rect(x, y, G.rect[rect]['size'][0]*G.w, G.rect[rect]['size'][1]*G.h)
+            pygame.draw.rect(G.window_surface, G.rect[rect]['color'], box)
         pygame.display.update()
         G.tick += 1
