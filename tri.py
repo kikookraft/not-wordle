e339d8d8876ebcf552783ebf34b78ff863ae114e
kikookraft <kikookraft.teamcraft@gmail.com>
2022-02-09 17:50:01 +0100

    Supression des mots trop long

--- tri.py
+++ tri.py
@@ -2,10 +2,12 @@
 l=[]
 
 with open("res/fr.txt", 'r') as f:
-    l.append(f.readline().rstrip())
+    for i in f:
+        i.rstrip()
+        l.append(i)
 
 with open("res/fr.txt", 'w') as f:
     for i in l:
-        if len(i)<10:
-            f.write(i+"\n")
+        if len(i)<10 and len(i)>4:
+            f.write(i)
             print(i)
