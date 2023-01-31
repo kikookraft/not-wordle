9fa1eb5d9f1f1c73fac387a2cd340b2b94948716
kikookraft <kikookraft.teamcraft@gmail.com>
2023-01-31 17:39:28 +0100

    reduction de la longueur des mots

--- res/tri.py
+++ res/tri.py
@@ -13,7 +13,7 @@ words = []
 
 for word in l:
         good = True
-        if len(word)<9 and len(word)>4: # garder les mots entre 5 et 8 charactères
+        if len(word)<7 and len(word)>4: # garder les mots entre 5 et 6 charactères
             for letter in word:
                 if letter not in "abcdefghijklmnopqrstuvwxyz": # garder uniquement les charactères classiques (ie. pas de àéèç...)
                     good = False
